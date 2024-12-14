581e8000a52684dd08e841e8e420182475701067
if230033 <r.adhikari@students.htl-leonding.ac.at>
2024-12-14 09:57:07 +0100

    test rust :D

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    /*let mut x: i32 = 64;
+    let y: i32 = 32;
+    let z: i32 = x + y;
+
+    x+= 1;
+
+    let x = "Hello"; // Shadowing*/
+
+   /* let number = "42";
+    let number: i32 = number.parse().unwrap();
+    println!("The value of number is: {}", number);*/
+
+    let mut numbers: Vec<i32> = vec![1, 2, 3, 4, 5];
+    numbers.push(6);
+    numbers.pop();
+}
